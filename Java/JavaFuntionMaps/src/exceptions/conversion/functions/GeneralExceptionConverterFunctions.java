76817d3fcd716727b9f7085ac0f741d99573d600
Pat Howell <patrick.howell@outlook.com>
2015-03-16 21:29:39 +0000

    Added infrastructure to allow implementation of functions to go into exception handling map

---
+++ Java/JavaFuntionMaps/src/exceptions/conversion/functions/GeneralExceptionConverterFunctions.java
@@ -0,0 +1,10 @@
+package exceptions.conversion.functions;
+
+/**
+ * Class containing exception handling methods for the GeneralExcetptionConverter implementation
+ * of ExceptionConverter. Methods here must conform to the interface:
+ * Function<Exception, SystemException>>
+ */
+public final class GeneralExceptionConverterFunctions {
+
+}
---
+++ Java/JavaFuntionMaps/src/exceptions/conversion/functions/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Package for the functions which handle exceptions to be created in. Classes should be
+ * indicate in their name the implementation of ExceptionConverter that their functions
+ * should be used by.
+ */
+package exceptions.conversion.functions;
